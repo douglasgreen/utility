cd503c377f342eef0e5bdafc590e0a36b2120169
Douglas S. Green <douglas@nurd.site>
2024-06-07 20:03:16 -0400

    feat: adding funcs

--- src/Dir.php
+++ src/Dir.php
@@ -27,4 +27,34 @@ class Dir
 
         return $result;
     }
+
+    /**
+     * Substitute for mkdir.
+     *
+     * @param ?resource $context
+     * @throws DirectoryException
+     */
+    public static function make(
+        string $directory,
+        int $permissions = 0o777,
+        bool $recursive = false,
+        $context = null
+    ): void {
+        if (mkdir($directory, $permissions, $recursive, $context) === false) {
+            throw new DirectoryException('Unable to make directory');
+        }
+    }
+
+    /**
+     * Substitute for rmdir.
+     *
+     * @param ?resource $context
+     * @throws DirectoryException
+     */
+    public static function remove(string $directory, $context = null): void
+    {
+        if (rmdir($directory, $context) === false) {
+            throw new DirectoryException('Unable to make directory');
+        }
+    }
 }
--- src/File.php
+++ src/File.php
@@ -10,27 +10,6 @@ use DouglasGreen\Utility\Exceptions\FileSystem\FileException;
  * File utility class to throw exceptions when basic operations fail.
  *
  * @todo Add other file functions from this list.
- * rewind
- * unlink
- * fwrite
- * file
- * touch
- * mkdir
- * fread
- * fseek
- * copy
- * chmod
- * tempnam
- * filesize
- * rename
- * rmdir
- * ftell
- * pathinfo
- * readfile
- * glob
- * symlink
- * fstat
- * filemtime
  * umask
  * fileperms
  * link
@@ -38,19 +17,66 @@ use DouglasGreen\Utility\Exceptions\FileSystem\FileException;
  * ftruncate
  * flock
  * readlink
- * chgrp
  * stat
- * chown
  * move_uploaded_file
  * disk_free_space
- * fpassthru
  * popen
  * pclose
  * lstat
  * parse_ini_file
+ *
+ * @SuppressWarnings(PHPMD.ExcessiveClassComplexity)
+ * @SuppressWarnings(PHPMD.TooManyPublicMethods)
  */
 class File
 {
+    /**
+     * Substitute for chgrp.
+     *
+     * @throws FileException
+     */
+    public static function changeGroup(
+        string $filename,
+        string|int $group
+    ): void {
+        if (chgrp($filename, $group) === false) {
+            throw new FileException(sprintf(
+                'Unable to change group of file "%s"',
+                $filename
+            ));
+        }
+    }
+
+    /**
+     * Substitute for chmod.
+     *
+     * @throws FileException
+     */
+    public static function changeMode(string $filename, int $permissions): void
+    {
+        if (chmod($filename, $permissions) === false) {
+            throw new FileException(sprintf(
+                'Unable to change mode of file "%s"',
+                $filename
+            ));
+        }
+    }
+
+    /**
+     * Substitute for chown.
+     *
+     * @throws FileException
+     */
+    public static function changeOwner(string $filename, string|int $user): void
+    {
+        if (chown($filename, $user) === false) {
+            throw new FileException(sprintf(
+                'Unable to change owner of file "%s"',
+                $filename
+            ));
+        }
+    }
+
     /**
      * Substitute for fclose.
      *
@@ -62,6 +88,72 @@ class File
         if (fclose($stream) === false) {
             throw new FileException('Unable to close file');
         }
+    }
+
+    /**
+     * Substitute for copy.
+     *
+     * @param ?resource $context
+     * @throws FileException
+     */
+    public static function copy(string $source, string $target, $context): void
+    {
+        if (copy($source, $target, $context) === false) {
+            throw new FileException(sprintf(
+                'Unable to copy file from "%s" to "%s"',
+                $source,
+                $target
+            ));
+        }
+    }
+
+    /**
+     * Substitute for unlink.
+     *
+     * @param ?resource $context
+     * @throws FileException
+     */
+    public static function delete(string $filename, $context): void
+    {
+        if (unlink($filename, $context) === false) {
+            throw new FileException('Unable to delete file');
+        }
+    }
+
+    /**
+     * Substitute for filesize.
+     *
+     * @throws FileException
+     */
+    public static function size(string $filename): int
+    {
+        $result = filesize($filename);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to get size of file "%s"',
+                $filename
+            ));
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for fileatime.
+     *
+     * @throws FileException
+     */
+    public static function getAccessTime(string $filename): int
+    {
+        $result = fileatime($filename);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to get last access time of file "%s"',
+                $filename
+            ));
+        }
+
+        return $result;
     }
 
     /**
@@ -84,7 +176,7 @@ class File
         // Distinguish between end-of-data false and error false.
         if ($fields === false) {
             if (! feof($stream)) {
-                throw new FileException('Unable to get line from file');
+                throw new FileException('Unable to get CSV line from file');
             }
 
             return null;
@@ -114,6 +206,64 @@ class File
         }
 
         return $buffer;
+    }
+
+    /**
+     * Substitute for filectime.
+     *
+     * @throws FileException
+     */
+    public static function getMetaChangeTime(string $filename): int
+    {
+        $result = filectime($filename);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to get last metadata change time of file "%s"',
+                $filename
+            ));
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for filemtime.
+     *
+     * @throws FileException
+     */
+    public static function getWriteTime(string $filename): int
+    {
+        $result = filemtime($filename);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to get last modification time of file "%s"',
+                $filename
+            ));
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for readfile.
+     *
+     * @param ?resource $context
+     * @throws FileException
+     */
+    public static function loadAndPrint(
+        string $filename,
+        bool $useIncludePath = false,
+        $context = null
+    ): int {
+        $result = readfile($filename, $useIncludePath, $context);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to load and print file "%s"',
+                $filename
+            ));
+        }
+
+        return $result;
     }
 
     /**
@@ -166,6 +316,43 @@ class File
     }
 
     /**
+     * Substitute for tempnam.
+     *
+     * @throws FileException
+     */
+    public static function makeTemp(string $directory, string $prefix): string
+    {
+        $result = tempnam($directory, $prefix);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to create temp file in directory "%s"',
+                $directory
+            ));
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for glob.
+     *
+     * @return list<string>
+     * @throws FileException
+     */
+    public static function findAll(string $pattern, int $flags = 0): array
+    {
+        $result = glob($pattern, $flags);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable to search files for pattern "%s"',
+                $pattern
+            ));
+        }
+
+        return $result;
+    }
+
+    /**
      * Substitute for fopen.
      *
      * @param resource $context
@@ -180,7 +367,10 @@ class File
     ) {
         $handle = fopen($filename, $mode, $useIncludePath, $context);
         if ($handle === false) {
-            throw new FileException(sprintf('Unable to open "%s"', $filename));
+            throw new FileException(sprintf(
+                'Unable to open file "%s"',
+                $filename
+            ));
         }
 
         return $handle;
@@ -204,6 +394,73 @@ class File
     }
 
     /**
+     * Substitute for fread.
+     *
+     * @param resource $stream
+     * @param int<1, max> $length
+     * @throws FileException
+     */
+    public static function read($stream, int $length): string
+    {
+        $result = fread($stream, $length);
+        if ($result === false) {
+            throw new FileException('Unable to read string from file');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for fpassthru. Assumes that 0 bytes read means failure.
+     *
+     * @param resource $stream
+     * @throws FileException
+     */
+    public static function readAndPrintRest($stream): int
+    {
+        $result = fpassthru($stream);
+        if ($result === 0) {
+            throw new FileException('Unable to read and print rest of file');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for rename.
+     *
+     * @param ?resource $context
+     * @throws FileException
+     */
+    public static function rename(
+        string $source,
+        string $target,
+        $context = null
+    ): void {
+        if (rename($source, $target, $context) === false) {
+            throw new FileException(sprintf(
+                'Unable to rename file from "%s" to "%s"',
+                $source,
+                $target
+            ));
+        }
+    }
+
+    /**
+     * Substitute for rewind.
+     *
+     * @param resource $stream
+     * @throws FileException
+     */
+    public static function rewind($stream): void
+    {
+        $result = rewind($stream);
+        if ($result === false) {
+            throw new FileException('Unable to rewind file');
+        }
+    }
+
+    /**
      * Substitute for file_put_contents.
      *
      * @param resource $context
@@ -218,6 +475,110 @@ class File
         $result = file_put_contents($filename, $data, $flags, $context);
         if ($result === false) {
             throw new FileException('Unable to save string to file');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for fseek.
+     *
+     * @param resource $stream
+     * @throws FileException
+     */
+    public static function seek(
+        $stream,
+        int $offset,
+        int $whence = SEEK_SET
+    ): void {
+        if (fseek($stream, $offset, $whence) === -1) {
+            throw new FileException('Unable to seek on file');
+        }
+    }
+
+    /**
+     * Substitute for fstat.
+     *
+     * @param resource $stream
+     * @return array<string|int, int>
+     * @throws FileException
+     */
+    public static function stats($stream): array
+    {
+        $result = fstat($stream);
+        if ($result === false) {
+            throw new FileException('Unable to get stats from file');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for symlink.
+     *
+     * @throws FileException
+     */
+    public static function symlink(string $target, string $link): void
+    {
+        if (symlink($target, $link) === false) {
+            throw new FileException(sprintf(
+                'Unable to link "%s" to file "%s"',
+                $link,
+                $target
+            ));
+        }
+    }
+
+    /**
+     * Substitute for ftell.
+     *
+     * @param resource $stream
+     * @throws FileException
+     */
+    public static function tell($stream): int
+    {
+        $result = ftell($stream);
+        if ($result === false) {
+            throw new FileException('Unable to tell file');
+        }
+
+        return $result;
+    }
+
+    /**
+     * Substitute for touch.
+     *
+     * @throws FileException
+     */
+    public static function touch(
+        string $filename,
+        ?int $mtime = null,
+        ?int $atime = null
+    ): void {
+        $result = touch($filename, $mtime, $atime);
+        if ($result === false) {
+            throw new FileException(sprintf(
+                'Unable set file access and modification times on file "%s"',
+                $filename
+            ));
+        }
+    }
+
+    /**
+     * Substitute for fwrite.
+     *
+     * @param resource $stream
+     * @param ?int<0, max> $length
+     * @throws FileException
+     */
+    public static function write(
+        $stream,
+        string $data,
+        ?int $length = null,
+    ): int {
+        $result = fwrite($stream, $data, $length);
+        if ($result === false) {
+            throw new FileException('Unable to write string to file');
         }
 
         return $result;
