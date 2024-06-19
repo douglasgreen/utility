60e51a37b55d0d7141e2fd408faad24648f4c509
Douglas S. Green <douglas@nurd.site>
2024-06-19 09:03:29 -0400

    refactor: Moving argument exception to Data namespace

--- src/Data/ArgumentException.php
+++ src/Data/ArgumentException.php
@@ -2,11 +2,11 @@
 
 declare(strict_types=1);
 
-namespace DouglasGreen\Utility\Logic;
+namespace DouglasGreen\Utility\Data;
 
 /**
  * Exception for invalid arguments passed to a method.
  * Example: A method expects a non-null string argument, but receives null or an
  * integer.
  */
-class ArgumentException extends ProcessException {}
+class ArgumentException extends DataException {}
